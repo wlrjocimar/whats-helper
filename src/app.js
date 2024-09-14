42e2ef414a456d6520762eecd4af2a6218db83a7
Jocimar Walter <jocimarwalter@bb.com.br>
2024-09-14 17:16:08 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const whatsappRoutes = require('./routes/whatsappRoutes');
 
 const app = express();
 
-app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/api/whatsapp', whatsappRoutes);
 
 module.exports = app;
