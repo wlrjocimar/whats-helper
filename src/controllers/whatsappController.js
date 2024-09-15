9e033dfea4f5887a22e72e0852f0bda1d35731d1
Jocimar Walter <jocimarwalter@bb.com.br>
2024-09-15 18:23:47 -0300

    ajuste

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -75,38 +75,38 @@ async function convertAudio(inputPath, outputPath) {
 
 
 
-async function quickstart() {
-    // The path to the remote LINEAR16 file
-    const gcsUri = 'gs://cloud-samples-data/speech/brooklyn_bridge.raw';
-  
-    // The audio file's encoding, sample rate in hertz, and BCP-47 language code
-    const audio = {
-      uri: gcsUri,
-    };
-    const config = {
-      encoding: 'LINEAR16',
-      sampleRateHertz: 16000,
-      languageCode: 'en-US',
-    };
-    const request = {
-      audio: audio,
-      config: config,
-    };
-  
-    try {
-        // Detects speech in the audio file
-    const [response] = await client.recognize(request);
-    const transcription = response.results
-      .map(result => result.alternatives[0].transcript)
-      .join('\n');
-    console.log(`Transcription: ${transcription}`);
-        
-    } catch (error) {
 
-        console.log("Erro ao transcrever",error)
-        
-    }
-    
+    async function quickstart() {
+        // Lê o arquivo de áudio local
+        const file = fs.readFileSync('converted_audio.flac');
+      
+        // Define o conteúdo do áudio como base64
+        const audio = {
+          content: file.toString('base64'),  // Converte o áudio para base64
+        };
+      
+        // Configuração para o arquivo FLAC
+        const config = {
+          encoding: 'FLAC',  // Define o formato do arquivo de áudio
+          sampleRateHertz: 16000,  // Taxa de amostragem (ajuste se necessário)
+          languageCode: 'en-US',  // Código de idioma (ajuste conforme necessário)
+        };
+      
+        const request = {
+          audio: audio,
+          config: config,
+        };
+      
+        try {
+          // Detecta fala no arquivo de áudio
+          const [response] = await client.recognize(request);
+          const transcription = response.results
+            .map(result => result.alternatives[0].transcript)
+            .join('\n');
+          console.log(`Transcription: ${transcription}`);
+        } catch (error) {
+          console.log('Erro ao transcrever:', error);
+        }
   }
   
 
