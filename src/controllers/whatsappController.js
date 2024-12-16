7b3d2213722a3ebb4270f7267dbedaa10b3b6f2e
Jocimar Walter <jocimarwalter@bb.com.br>
2024-12-16 07:48:50 -0300

    incluido status no filtro

--- src/controllers/whatsappController.js
+++ src/controllers/whatsappController.js
@@ -812,6 +812,10 @@ exports.receiveMessageOfficialApiPost = async (req, res) => {
     if (messages && messages.length > 0) {
         const message = messages[0];
         const from = message.from;
+
+        const responseMessage = "Desculpe mas esta mensagem não pode ser enviada a nossa assistente. Caso ainda necessite desta resposta peço por gentileza que refaça a pergunta";
+                 await sendReplyToMessage(from, responseMessage, message.id);  // Envia a resposta com referência à mensagem original
+                 return;
         
 
 
